4602dca9731e072a2bef0955860d23a9f2f89b2a
WWWolverine <tevrizovvv@gmail.com>
2022-08-02 21:44:39 +0600

    media

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <Timer restart={restart} currentPlayer={currentPlayer} />
+      {/* <Timer restart={restart} currentPlayer={currentPlayer} /> */}
       <BoardComponent
         board={board}
         setBoard={setBoard}
